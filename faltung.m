73e61711727d3ce9ce045d250531122613c00803
Sanngrithr <34239794+Sanngrithr@users.noreply.github.com>
2018-12-10 14:22:53 +0100

    Add files via upload

--- faltung.m
+++ faltung.m
@@ -1,14 +1,12 @@
 function ausgangssignal = faltung(eingangssignal, impulsantwort)
 
-ausgangssignal = zeros(1,size(impulsantwort,2)+1);
+ausgangssignal = zeros(1,size(impulsantwort,2)+size(eingangssignal,2));
 
 for t = 1:size(ausgangssignal,2)
-   for u = 1:size(impulsantwort,2)
-       if((t-u) >= 1 && (t-u)<= size(eingangssignal,2) && t<= size(ausgangssignal,2) && u <= size(impulsantwort,2))
-       ausgangssignal(t) = ausgangssignal(t)+(eingangssignal(t-u) * impulsantwort(u));
-       else
-           
-       end
-   end
+ for u = 1:size(impulsantwort,2)
+  if((t-u) >= 1 && (t-u)<= size(eingangssignal,2) && t<= size(ausgangssignal,2) && u <= size(impulsantwort,2))
+       ausgangssignal(t) = ausgangssignal(t)+(eingangssignal(t-u) * impulsantwort(u));      
+  end
+ end
 end
-end
+ausgangssignal=ausgangssignal(2:end);
